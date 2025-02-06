a003ae3799de542b21e25d3af8e74d6ef30ec046
Nikhil P S <nikhilpsathyanathan@gmail.com>
2025-02-06 18:14:37 +0530

    Add user authentication redirect to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,15 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
+export const dynamic = "force-dynamic";
 
 const Dashboard = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+
+  if (!user) {
+    redirect("/");
+  }
   return (
     <div className="flex items-center justify-center h-screen">
       <div>{user.email}</div>;
